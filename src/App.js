895e9b36978c9c2da0571af2ee2108076387620a
Tahir Taim <syedraihanali65@gmail.com>
2023-05-22 12:15:31 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import ReactGA from 'react-ga'
 
 import { Analytics } from '@vercel/analytics/react';
+import { useEffect } from "react";
 
 
 
 
 function App() {
+
+  ReactGA.initialize('UA-268974325-1')
+
+  useEffect(()=>{
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  },[])
+
   return (
     
     <div>
